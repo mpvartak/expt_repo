1b8e6548751ca52f8223499fdfe5962acf37a575
Manasi Vartak <mpvartak@gmail.com>
2016-11-28 22:51:43 -0500

    running experiment

---
+++ code/BasicWorkflow.py
@@ -0,0 +1,44 @@
+from modeldb.basic.ModelDbSyncerBase import *
+import sys
+
+# syncer_obj = Syncer.create_syncer("gensim test", "test_user", \
+#     "using modeldb light logging")
+
+# syncer_obj = Syncer.create_syncer_from_config(
+#     "/Users/mvartak/Projects/modeldb_test_dir/dir/.mdb_config")
+
+# syncer_obj = Syncer(
+#     NewOrExistingProject("gensim test", "test_user",
+#     "using modeldb light logging"),
+#     DefaultExperiment(),
+#     NewExperimentRun("", "A1B2C3"))
+
+experiment_run_id = int(sys.argv[len(sys.argv) - 1])
+syncer_obj = Syncer.create_syncer_for_experiment_run(experiment_run_id)
+
+print "I'm training some model"
+
+datasets = {
+    "train" : Dataset("/path/to/train", {}),
+    "test" : Dataset("/path/to/test", {})
+}
+model ="model_obj"
+mdb_model1 = Model("NN", model, "/path/to/model1")
+model_config1 = ModelConfig("NN", {"l1" : 10})
+model_metrics1 = ModelMetrics({"accuracy" : 0.8})
+
+mdb_model2 = Model("NN", model, "/path/to/model2")
+model_config2 = ModelConfig("NN", {"l1" : 20})
+model_metrics2 = ModelMetrics({"accuracy" : 0.9})
+
+syncer_obj.sync_datasets(datasets)
+
+syncer_obj.sync_model("train", model_config1, mdb_model1)
+syncer_obj.sync_metrics("test", mdb_model1, model_metrics1)
+
+syncer_obj.sync_model("train", model_config2, mdb_model2)
+syncer_obj.sync_metrics("test", mdb_model2, model_metrics2)
+
+syncer_obj.sync()
+
+
---
+++ code/build.sbt
@@ -0,0 +1,10 @@
+name := "Model DB Spark Client Sample"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.0" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.0" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.0.0" % "provided"
